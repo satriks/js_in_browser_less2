56da75a9b8ac25f24ca0eb4b56a4428b67ea78c1
Yury Vereskun <satriks@mail.ru>
2023-06-08 18:00:13 +0300

    add slider

---
+++ slider/task.js
@@ -0,0 +1,38 @@
+const sliderItem = Array.from(document.querySelectorAll('.slider__item'))
+const sliderDot = Array.from(document.querySelectorAll('.slider__dot'))
+
+document.querySelector('.slider__arrow_prev').addEventListener('click', prevSlide)
+document.querySelector('.slider__arrow_next').addEventListener('click', nextSlide)
+sliderDot.forEach(x => x.addEventListener('click', pickSlide))
+let ind = 0
+
+function slideOn (ind) {
+    sliderItem[ind].classList.toggle('slider__item_active')
+    sliderDot[ind].classList.toggle('slider__dot_active')
+}
+
+function checkSlide () {
+    ind = sliderItem.findIndex(x => x.matches('.slider__item_active'))
+    slideOn(ind)
+}
+
+function nextSlide (){
+    checkSlide()
+    if ( ind + 1 == sliderItem.length ) {
+        ind = -1
+    }
+    slideOn(ind +1)
+}
+
+function prevSlide (){
+    checkSlide()
+    if ( ind - 1 <  0  ) {
+        ind = sliderItem.length 
+    }
+    slideOn(ind -1)
+}
+
+function pickSlide () {
+    checkSlide()
+    slideOn (sliderDot.indexOf(this))
+}
